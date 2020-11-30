c07c785f2c281e413589b3ec5aa529de7261bfa7
Paul Stroot <paul@dancingpaul.com>
2020-11-30 14:35:55 -0600

    Uncomment creation of all page types. Fix syntax bug in parallax_example.js. Added fonts.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,23 +8,23 @@ const { createRemoteFileNode } = require("gatsby-source-filesystem")
 
 
 const createPages = require(`./gatsby/createPages`)
-//const createPosts = require(`./gatsby/createPosts`)
-//const createCategories = require(`./gatsby/createCategories`)
-//const createSolutions = require(`./gatsby/createSolutions`)
-//const createProducts = require(`./gatsby/createProducts`)
-//const createCasestudies = require(`./gatsby/createCasestudies`)  
-//const createTags = require(`./gatsby/createTags`)
+const createPosts = require(`./gatsby/createPosts`)
+const createCategories = require(`./gatsby/createCategories`)
+const createSolutions = require(`./gatsby/createSolutions`)
+const createProducts = require(`./gatsby/createProducts`)
+const createCasestudies = require(`./gatsby/createCasestudies`)  
+const createTags = require(`./gatsby/createTags`)
 //const createUsers = require(`./gatsby/createUsers`)
 
 exports.createPages = async ({ actions, graphql }) => {
   await createPages({ actions, graphql })
-  //await createPosts({ actions, graphql })
-  //await createCategories({ actions, graphql })
-  //await createSolutions({ actions, graphql })
-  //await createProducts({ actions, graphql })
-  //await createCasestudies({ actions, graphql })
-  //await createTags({ actions, graphql })
-  //await createUsers({ actions, graphql })
+  await createPosts({ actions, graphql })
+  await createCategories({ actions, graphql })
+  await createSolutions({ actions, graphql })
+  await createProducts({ actions, graphql })
+  await createCasestudies({ actions, graphql })
+  await createTags({ actions, graphql })
+ // await createUsers({ actions, graphql })
 }
 
 
