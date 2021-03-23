59914d7b2b53e0db0960d5c6c997e3f20a9a6acc
Jon Burr <jonathanwburr@gmail.com>
2021-03-23 16:30:11 -0700

    update footer social nav logic

--- src/common/ui/menus/SocialMenu.jsx
+++ src/common/ui/menus/SocialMenu.jsx
@@ -1,34 +1,22 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import FacebookFillIcon from 'remixicon-react/FacebookFillIcon';
-import TwitterFillIcon from 'remixicon-react/TwitterFillIcon';
-import LinkedinFillIcon from 'remixicon-react/LinkedinFillIcon';
-import MailFillIcon from 'remixicon-react/MailFillIcon';
+import parse from "html-react-parser"
+import { useSiteFooter } from '../../hooks/useSiteFooter'
+
 
 const SocialMenu = () => {
+  const { options } = useSiteFooter()
   return (
     <>
       <div className="socialnav d-flex">
-        <div className="icon-circle-bg mr-3">
-          <TwitterFillIcon size="14" />
-        </div>
-        <div className="icon-circle-bg mr-3">
-          <FacebookFillIcon size="14" />
-        </div>
-        <div className="icon-circle-bg mr-3">
-          <LinkedinFillIcon size="14" />
-        </div>
-        <div className="icon-circle-bg mr-3">
-          <MailFillIcon size="14" />
-        </div>
-      </div>
-      {/* <div className="socialnav">
         {
-          options.socialLinks.map(({ site,url,svgCode },index) => (
-            <a href={url} title={site} className="mr-3" key={`social-${index}`} dangerouslySetInnerHTML={{ __html: svgCode }}></a>
+          options.socialLinks.map(({ site,url,svgCode }, index) => (
+            <a href={url} title={site} target="_blank" rel="noreferrer" className="mr-3 icon-circle-bg" key={`social-${index}`}>
+              {parse(svgCode)}
+            </a>
           ))
         }
-      </div> */}
+      </div>
     </>
   )
 }
