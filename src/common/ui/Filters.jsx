e332284cb578df707f870a37f53ec0692b3f47c5
tombeck <t@ocupop.com>
2021-03-31 10:25:18 -0700

    adjust links on filters

--- src/common/ui/Filters.jsx
+++ src/common/ui/Filters.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 // import PropTypes from 'prop-types'
 
 const Filters = () => {
@@ -6,13 +7,13 @@ const Filters = () => {
     <div className="d-flex align-items-start my-5">
       <h5 className="text-uppercase">Filter news by:</h5>
       <div className="col pb-2 border-left border-primary ml-3">
-        <h5 className="text-primary text-uppercase">Press Releases</h5>
+        <Link to="/press-releases" className="h5 text-primary text-uppercase">Press Releases</Link>
       </div>
       <div className="col pb-2 border-left border-primary ml-3">
-        <h5 className="text-primary text-uppercase">In The News</h5>
+        <Link to="/in-the-news" className="h5 text-primary text-uppercase">In The News</Link>
       </div>
       <div className="col pb-2 border-left border-primary ml-3">
-        <h5 className="text-primary text-uppercase">Events</h5>
+        <Link to="/events" className="h5 text-primary text-uppercase">Events</Link>
       </div>
     </div>
   )
