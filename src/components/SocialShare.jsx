735a903239698b6c5d0b8508571e4b3ec879ff39
Jon Burr <jonathanwburr@gmail.com>
2021-04-05 15:57:23 -0700

    socialnav for pages

--- src/components/SocialShare.jsx
+++ src/components/SocialShare.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 import SocialNav from './SocialNav'
+import SocialMenu from '../common/ui/menus/SocialMenu'
 
 const SocialShare = () => {
   return (
     <>
       <div className="border-bracket">
         <p className="mt-3 mb-0">Share this story.</p>
-        <SocialNav />
+        {/* <SocialNav /> */}
+        <SocialMenu id="socialshare-page" />
       </div>
     </>
   )
