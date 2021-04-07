a669141c6513dba03382179c0a2f08aa20ee2ae1
tombeck <t@ocupop.com>
2021-04-07 15:06:07 -0700

    add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,34 @@ module.exports = {
     'gatsby-plugin-recaptcha',
     // 'gatsby-plugin-sitemap',
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-W3BZNJ2",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        // defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
+    },
+    {
       /**
        * First up is the WordPress source plugin that connects Gatsby
        * to your WordPress site.
