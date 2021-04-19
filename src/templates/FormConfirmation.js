7c10c911de2e954dc2ccac21c4ae68a6269797c1
Jon Burr <jonathanwburr@gmail.com>
2021-04-19 14:08:07 -0700

    Update FormConfirmation.js

--- src/templates/FormConfirmation.js
+++ src/templates/FormConfirmation.js
@@ -40,10 +40,10 @@ const FormConfirmationTemplate = ({ data: { post } }) => {
                 <>
                   <hr className="hr-styled" />
                   <div className="responsive-iframe aspect-letter my-5">
-                    <iframe src={acf.document.link} title={post.title} />
+                    <iframe src={embedUrl(acf.document.localFile.url)} title={post.title} />
                   </div>
                   <div className="text-center">
-                    <a href={acf.document.link} title="Download" target="_blank" rel="noreferrer" className="btn btn-primary mx-auto text-white">Download</a>
+                    <a href={acf.document.localFile.url} title="Download" target="_blank" rel="noreferrer" className="btn btn-primary mx-auto text-white">Download</a>
                   </div>
                 </>
               )}
@@ -72,6 +72,9 @@ export const formConfirmationQuery = graphql`
       acfPostTypeFormConfirmation {
         document {
           link
+          localFile {
+            url
+          }
         }
         videoEmbed
       }
