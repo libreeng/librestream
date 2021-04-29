c1386768b8d411a4850af994f0559ac3a62e4c64
tombeck <t@ocupop.com>
2021-04-29 15:33:57 -0700

    #104 Fix for image data source on use cases page

--- src/templates/standalone/UseCases.js
+++ src/templates/standalone/UseCases.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import parse from "html-react-parser"
+import { BgImage } from "gbimage-bridge"
+import { getImage } from 'gatsby-plugin-image'
 import SEO from "../../containers/SEO"
 import { useCaseStudies } from "../../common/hooks/useCaseStudies"
 import Hero from "../../common/ui/Hero"
 import FooterCTAs from '../../common/ui/FooterCTAs'
 import Layout from "../../containers/Layout"
+
 
 const UseCasesTemplate = ({ data: { page } }) => {
   const { caseStudies } = useCaseStudies()
@@ -38,6 +41,7 @@ const UseCasesTemplate = ({ data: { page } }) => {
               const url = externalLink
                 ? externalLink.url
                 : uri
+              const featuredImageData = getImage(featuredImage?.localFile)
 
               return (
                 <div key={id} className="col-12 col-sm-6 col-lg-4">
@@ -47,16 +51,16 @@ const UseCasesTemplate = ({ data: { page } }) => {
                     rel={externalLink ? 'noopener' : ''}
                   >
                     <div className='card p-2'>
-                      <div
+                      <BgImage
                         className="card-img-top bg-image aspect-1x1 grayscale"
-                        style={featuredImage && { backgroundImage: `url(${featuredImage?.sourceUrl})` }}
+                        image={featuredImageData}
                       >
                         <div className="bg-fill">
                           {logoImage && (
                             <img src={logoImage.sourceUrl} className="img-fluid w-50" alt={title} />
                           )}
                         </div>
-                      </div>
+                      </BgImage>
                       <div>
                         <p>
                           <span className="text-dark">{caption || title}</span><br />
