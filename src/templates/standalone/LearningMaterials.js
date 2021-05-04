908f02604c3d1ec8fa8f87f407873e968f682820
Paul Stroot <paul@dancingpaul.com>
2021-05-04 15:40:35 -0500

    Revert "Minor update."
    This reverts commit c3d3a1d064b63d992b614c7faf04e0b235e8eb88.

--- src/templates/standalone/LearningMaterials.js
+++ src/templates/standalone/LearningMaterials.js
@@ -26,7 +26,6 @@ const LearningMaterialsTemplate = ({ data: { page } }) => {
       <section>
         <div className="container">
           <div className="row">
-            TEST Preview Bug
             <div className="col-12 col-lg-4">
               <LearningNav sections={materials} />
             </div>
@@ -84,7 +83,6 @@ export const pageQuery = graphql`query LearningMaterialsTemplateQuery($id: Strin
                   page {
                     ... on WpPage {
                       uri
-                      link
                     }
                   }
                   videoEmbed
