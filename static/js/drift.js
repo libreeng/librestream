8311bebb9aa65457008204751d93a6008ea129ce
Paul Stroot <paul@dancingpaul.com>
2021-05-12 14:44:20 -0500

    Change drift error to warning.

--- static/js/drift.js
+++ static/js/drift.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', function(e) {
     !function () {
       var t = window.driftt = window.drift = window.driftt || [];
       if (!t.init) {
-        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+        if (t.invoked) return void (window.console && console.warn && console.warn("Drift snippet included twice."));
         t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
           t.factory = function (e) {
             return function () {
