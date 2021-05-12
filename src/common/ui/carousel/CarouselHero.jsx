ca964c0d61605fffae287cdf7b78a66dce1c2be3
Paul Stroot <paul@dancingpaul.com>
2021-05-12 15:48:45 -0500

    Minor adjustment for testing

--- src/common/ui/carousel/CarouselHero.jsx
+++ src/common/ui/carousel/CarouselHero.jsx
@@ -1,28 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Fade } from "react-slideshow-image"
-import { BgImage } from 'gbimage-bridge'
 
 const CarouselHero = ({ images, config, content }) => {
-  const settings = {
-    duration: 5000,
-    autoplay: true,
-    transitionDuration: 1000,
-    arrows: false,
-    infinite: true,
-    easing: "ease",
-    ...config
-  }
+ 
 
   return (
     <>
-      <div></div>
+      <div>THIS IS THE HERO AREA --- No Carousel. </div>
     </>
   )
 }
 
-CarouselHero.propTypes = {
-  images: PropTypes.instanceOf(Array)
-}
+
 
 export default CarouselHero
