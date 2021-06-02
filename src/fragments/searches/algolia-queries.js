c18f661549e22b510f0717568d8a0a32eb9c6155
Paul Stroot <paul@dancingpaul.com>
2021-06-02 14:32:42 -0500

    Algolia Search Work in Progress.

--- src/fragments/searches/algolia-queries.js
+++ src/fragments/searches/algolia-queries.js
@@ -10,16 +10,36 @@ function postToAlgoliaRecord({ node: { id, acfPostTypeNews, tags, seo, ...rest }
     ...rest,
   }
 }
-function pageToAlgoliaRecord({ node: { id, acfHero, acfIntro, template, seo, ...rest } }) {
-  return {
-    objectID: id,
-    ...acfHero,
-    ...acfIntro,
-    ...template,
-    ...seo,
-    ...rest,
+
+
+function siteToAlgoliaRecord(data) {
+  // TO DO: finish this tranformer function to make one, big, magical object to return with all search results.
+  console.log("********************************************")
+  console.log("********************************************")
+  console.log("********************************************")
+  console.log("ALL DATA",data)
+
+  let output = [];
+  for(const page in data.pages.edges){
+    console.log(page.node)
+    console.log(page.node.id)
+    output.push ({
+      objectID: page.node.id,
+      nodeType: 'page'
+    })
   }
+  for(const post in data.posts){
+    output.push ({
+      objectID: post.node.id,
+      nodeType: 'page'
+    })
+  }
+  console.log("TRANSFORMED DATA",output)
+
+  return output
 }
+
+ 
 
 const queries = [
   {
@@ -33,11 +53,11 @@ const queries = [
   },
   {
     query: fs.readFileSync(
-      path.resolve(__dirname, 'algolia-pages.graphql'),
+      path.resolve(__dirname, 'algolia-site.graphql'),
       'utf-8',
     ),    
-    transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
-    indexName: 'LibrestreamPages',
+    transformer: ({ data }) => siteToAlgoliaRecord(data),
+    indexName: 'LibrestreamSite',
     settings: { attributesToSnippet: ['*:80'] },
   },
 ]
