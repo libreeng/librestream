6a532ca288e558f1ad8bbf9d74abcea3bb2c73e2
Paul Stroot <paul@dancingpaul.com>
2021-06-22 13:53:31 -0500

    Set requestConcurrency to be an environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
           MediaItem: {
             localFile: {
               maxFileSizeBytes: 1048576000, // 1GB
-              requestConcurrency: 5, // Default 100. Amount of images to download concurrently. Try lowering this if wordpress server crashes on import.
+              requestConcurrency: process.env.WPGRAPHQL_MEDIA_REQUEST_CONCURRENCY || 100, // Default 100. Amount of images to download concurrently. Try lowering this if wordpress server crashes on import.
             },
           },
         },
