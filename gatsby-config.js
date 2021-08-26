2cfafe54f6637e03b2550b6b45d8c1e0c5824956
tbeck <t@ocupop.com>
2021-08-26 08:53:17 -0700

    reconfigure gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    // `gatsby-plugin-recaptcha`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
